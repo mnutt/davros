e1f912369c4dd07e831dbfbf63ab2d1865235251
Michael Nutt <michael@nuttnet.net>
2015-07-11 16:39:05 -0400

    add jsdav and mount webdav to /remote.php/webdav

---
+++ server/index.js
@@ -0,0 +1,41 @@
+// To use it create some files under `mocks/`
+// e.g. `server/mocks/ember-hamsters.js`
+//
+// module.exports = function(app) {
+//   app.get('/ember-hamsters', function(req, res) {
+//     res.send('hello');
+//   });
+// };
+
+var path = require('path');
+var jsDAV = require("jsdav/lib/jsdav");
+var Tree = require("jsdav/lib/DAV/backends/fsext/tree");
+jsDAV.debugMode = true;
+var jsDAV_Locks_Backend_FS = require("jsdav/lib/DAV/plugins/locks/fs");
+
+module.exports = function(app) {
+  var root = path.resolve(__dirname + "/../data");
+
+  console.log("Mounting webdav from data dir " + root);
+
+  var server = jsDAV.mount({
+    node: root,
+    tree: Tree.new(root),
+    locksBackend: jsDAV_Locks_Backend_FS.new(root)
+  });
+
+  server.baseUri = '/remote.php/webdav';
+
+  app.use(function(req, res, next) {
+    if(req.url.indexOf(server.baseUri) === 0) {
+      server.emit('request', req, res);
+    } else {
+      next();
+    }
+  });
+
+  // Log proxy requests
+  var morgan  = require('morgan');
+  app.use(morgan('dev'));
+
+};
