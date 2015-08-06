e94afef928ef832fe9dc3838fd9ba36d2f3cad85
Michael Nutt <michael@nuttnet.net>
2015-08-05 23:57:31 -0400

    correctly navigate file hierarchy

--- app/models/file.js
+++ app/models/file.js
@@ -4,13 +4,18 @@ export default DS.Model.extend({
   mode: DS.attr('number'),
   path: DS.attr('string'),
   size: DS.attr('number'),
+  name: DS.attr('string'),
   files: DS.hasMany('files', {async: true}),
 
   parent: function() {
-    var parentDir = this.get('path').replace(/[^\/]*$/, '');
-    if(parentDir == "") {
+    var parentDir = this.get('path').replace(/[^\/]*\/?$/, '');
+    if(parentDir === "") {
       return "/";
     }
     return parentDir;
-  }.property('path')
+  }.property('path'),
+
+  isDirectory: function() {
+    return this.get('mode') === 16877;
+  }.property('mode')
 });
--- server/api.js
+++ server/api.js
@@ -22,36 +22,61 @@ module.exports = function(root) {
       return next();
     }
 
-    dirStat(fullPath, function(err, files) {
+    fs.stat(fullPath, function(err, stat) {
       if(err) {
         res.writeHead(500, {});
-        res.end(JSON.stringify(err));
+        res.send(JSON.stringify(err));
         return;
       }
+      console.log(stat);
 
-      files.forEach(function(file) {
-        file.path = relative(file.filePath);
-      });
+      var data = stat;
+      data.path = relativePath.slice(1);
+      data.name = path.basename(relativePath);
 
-      var data = {
-        path: relativePath.slice(1),
-        files: files
-      };
+      if(stat.isDirectory()) {
+        dirStat(fullPath, function(err, files) {
+          if(err) {
+            res.writeHead(500, {});
+            res.end(JSON.stringify(err));
+            return;
+          }
 
-      JSONAPISerializer('file', data, {
-        topLevelLinks: { self: 'http://localhost:4214/api/files' },
-        id: 'path',
-        attributes: ['path', 'files'],
-        files: {
-          ref: function(collection, field) {
-            return field.path;
-          },
-          attributes: ['size', 'ctime', 'path', 'atime', 'mode']
-        }
-      }).then(function(files) {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(files, null, 2));
-      });
+          files.forEach(function(file) {
+            file.path = relative(file.filePath);
+            file.name = path.basename(file.filePath);
+          });
+
+          data.files = files;
+
+          JSONAPISerializer('file', data, {
+            topLevelLinks: { self: 'http://localhost:4214/api/files' },
+            id: 'path',
+            attributes: ['path', 'files', 'name', 'size', 'mode', 'ctime', 'atime'],
+            files: {
+              ref: function(collection, field) {
+                return field.path;
+              },
+              attributes: ['size', 'ctime', 'path', 'atime', 'mode', 'name']
+            }
+          }).then(function(files) {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(files, null, 2));
+          });
+        });
+      } else {
+        var data = stat;
+
+        JSONAPISerializer('file', data, {
+          topLevelLinks: { self: 'http://localhost:4214/api/files' },
+          id: 'path',
+          attributes: ['size', 'ctime', 'path', 'atime', 'mode', 'name', 'is_directory']
+        }).then(function(file) {
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          res.end(JSON.stringify(file, null, 2));
+        });
+      }
     });
+
   };
 };
