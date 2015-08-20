aeb9c9e2ab8229756f93e8c5b4d5229bf53f4ab0
Michael Nutt <michael@nuttnet.net>
2015-08-19 23:39:59 -0400

    fix lint warnings

--- app/components/path-breadcrumb.js
+++ app/components/path-breadcrumb.js
@@ -1,3 +1,5 @@
+import Ember from 'ember';
+
 export default Ember.Component.extend({
   classNames: ['breadcrumb'],
 
--- app/routes/files.js
+++ app/routes/files.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(params) {
+  model: function() {
     return this.store.find('file', '/');
   },
 
