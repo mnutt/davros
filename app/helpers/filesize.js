7f1b21f6f3aef32a5208e080ccf03a31afbd4597
Michael Nutt <michael@nuttnet.net>
2015-08-25 23:33:39 -0400

    upgrade ember-paper and switch from list to table

---
+++ app/helpers/filesize.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Handlebars.makeBoundHelper(function(value) {
+  if (typeof value === 'undefined') {
+    return null;
+  }
+  var i,
+      filesize,
+      units = ['B', 'KB', 'MB', 'GB', 'TB'];
+  for (i = 0; i < units.length; i++) {
+    if (value < 1024) {
+      var unitValue = '<span>' + units[i] + '</span>';
+      return new Ember.Handlebars.SafeString(Math.floor(value) + unitValue);
+    }
+    value = value / 1024;
+  }
+  return filesize;
+});
