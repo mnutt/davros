581abc30e1da0a3f06038f1bdd4f515b118f4f5b
Michael Nutt <michael@nuttnet.net>
2015-10-24 18:27:08 -0400

    add clients page with connection instructions using sandstorm offer template

---
+++ app/components/offer-iframe.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['offer-iframe'],
+  tagName: 'iframe',
+  attributeBindings: ['src'],
+  src: '',
+
+  replacedTemplate: function() {
+    var template = this.get('template');
+    return template.replace('$API_PROTO', document.location.protocol);
+  }.property('template'),
+
+  fillIframe: function() {
+    window.parent.postMessage({
+      renderTemplate: {
+        rpcId: this.get('elementId'),
+        template: this.get('replacedTemplate')
+      }
+    }, "*");
+  }.on('didInsertElement'),
+
+  registerMessageListener: function() {
+    this.$(window).on('message', this.messageListener.bind(this));
+  }.on('willInsertElement'),
+
+  messageListener: function(e) {
+    var event = e.originalEvent;
+    if (event.data && event.data.rpcId === this.get('elementId')) {
+      if (event.data.error) {
+        console.log("Offer template error: " + event.data.error);
+      } else {
+        this.set('src', event.data.uri);
+      }
+    }
+  }
+});
