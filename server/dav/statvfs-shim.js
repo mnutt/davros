3b47d8d915da1bd2268dcc9312c264ad099ea26a
Michael Nutt <michael@nuttnet.net>
2015-11-02 11:18:25 -0500

    use 'du' to determine actual size of shared directory

--- server/dav/statvfs-shim.js
+++ server/dav/statvfs-shim.js
@@ -21,14 +21,27 @@ module.exports = function(p, cb) {
       var total = parseInt(match[1], 10) || 1;
       var free = parseInt(match[2], 10) || 0;
 
-      return cb(null, {
-        bsize: 1, // we specifically set the block size to 1k with `-k` above
-        frsize: 1, // doesn't really matter for our purposes
-        bfree: free * 1024, // how much of it is available, total
-        bavail: free * 1024, // how much available for unprivileged users, assume we get it
-        blocks: total * 1024, // total fs size
-        files: 0, // we don't support # of inodes at this time
-        ffree: 0 // nor this
+      exec("du -k -d 0 " + resolvedPath, function(err, stdout, stderr) {
+        if(err) {
+          return cb(err, null);
+        }
+
+        var match = stdout.match(/^(\d+)/);
+        if(!match) {
+          return cb("Failed to fetch used disk space", null);
+        }
+
+        var used = parseInt(match[1], 10) || 0;
+
+        return cb(null, {
+          bsize: 1, // we specifically set the block size to 1k with `-k` above
+          frsize: 1, // doesn't really matter for our purposes
+          bfree: 0, // no idea what this is for
+          bavail: free * 1024, // how much available for unprivileged users, assume we get it
+          blocks: used * 1024, // total fs size
+          files: 0, // we don't support # of inodes at this time
+          ffree: 0 // nor this
+        });
       });
     });
   });
