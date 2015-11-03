5716b33955308dedcde7ffca75f72b6b9bf1c70d
Michael Nutt <michael@nuttnet.net>
2015-11-02 23:40:53 -0500

    in case of error (or not running inside sandstorm) fail safe and show all permissions

--- app/services/permissions.js
+++ app/services/permissions.js
@@ -3,14 +3,19 @@ import ajax from 'ic-ajax';
 
 export default Ember.Service.extend({
   list: [],
+  error: false,
 
   init: function() {
     ajax("/api/permissions").then((result) => {
       this.set('list', result.permissions);
+    }).catch((err) => {
+      this.set('error', err);
+      this.set('list', []);
     });
   },
 
   can: function(permission) {
+    if(this.get('error')) { return true; } // fail safe in case permissions not available
     return this.get('list').indexOf(permission) >= 0;
   }
 });
