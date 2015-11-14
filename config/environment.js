47d303973c8d2a61ad13a4dd806d89dc590d7f69
Michael Nutt <michael@nuttnet.net>
2015-11-14 00:52:22 -0500

    whitelist some csp

--- config/environment.js
+++ config/environment.js
@@ -13,6 +13,15 @@ module.exports = function(environment) {
       }
     },
 
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self'",
+      'img-src': "'self' data:",
+      'media-src': "'self'"
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
