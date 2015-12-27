8c7f084ce822646070db96e8c8dd46f0831d9725
Michael Nutt <michael@nuttnet.net>
2015-12-27 16:14:22 -0500

    only add sample files if storage directory is missing
    We create the storage directory on first run and it can't be (permanently) removed, so there is no way the sample files will accidentally be re-added. Deleting the top level directory temporarily removes the storage directory, but node immediately adds it back, empty.

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -6,10 +6,11 @@
 #
 cd /opt/app
 
-mkdir -p $STORAGE_PATH
-mkdir -p $TEMP_STORAGE_PATH
-
 # Add some sample files if the grain is empty
-find $STORAGE_PATH -empty -maxdepth 0 -exec cp -r sample-files/* $STORAGE_PATH/ \;
+if [ ! -d "$STORAGE_PATH" ]; then
+  mkdir -p $TEMP_STORAGE_PATH
+  mkdir -p $STORAGE_PATH
+  cp -r sample-files/* $STORAGE_PATH/
+fi
 
 /usr/bin/node app.js
