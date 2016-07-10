002e6735ae7999d9bc774e469aad341e872ba7cc
Michael Nutt <michael@nuttnet.net>
2016-07-10 00:39:09 -0400

    add server tests

---
+++ tests/server/basic.js
@@ -0,0 +1,77 @@
+const assert  = require('chai').assert;
+const request = require('supertest');
+const support = require('./support');
+
+describe('GET directory', function() {
+  let server = support.makeApp();
+
+  describe('empty directory', function() {
+    it('returns an empty directory listing', function(done) {
+      request(server)
+        .propfind('/remote.php/webdav')
+        .set('content-type', 'application/xml')
+        .expect(function(res) {
+          let listing = support.directoryListing(res);
+          assert.equal(listing.length, 1);
+          assert.equal(listing[0].href, '/remote.php/webdav/');
+        })
+        .expect(207, done);
+    });
+  });
+
+  describe('with a file', function() {
+    it('returns directory listing', function(done) {
+      request(server)
+        .put('/remote.php/webdav/foo.txt')
+        .send({foo: 'foobar'})
+        .expect(200, function() {
+          request(server)
+            .propfind('/remote.php/webdav')
+            .set('content-type', 'application/xml')
+            .expect(function(res) {
+              let listing = support.directoryListing(res);
+              assert.equal(listing.length, 2);
+              assert.equal(listing[1].href, '/remote.php/webdav/foo.txt');
+            })
+            .expect(207, done);
+        });
+    });
+  });
+});
+
+describe('PUT file', function() {
+  let server = support.makeApp();
+
+  it('changes the directory etag', function(done) {
+    request(server)
+      .put('/remote.php/webdav/foo.txt')
+      .send({foo: 'foobar'})
+      .end(function(err) {
+        request(server)
+          .propfind('/remote.php/webdav')
+          .end(function(err, res) {
+            let listing = support.directoryListing(res);
+            let etag = listing[0].etag;
+            assert.ok(etag, 'initial directory listing should have etag set');
+
+            setTimeout(function() {
+            request(server)
+              .put('/remote.php/webdav/bar.txt')
+              .send({foo: 'foobar'})
+              .expect(200, function() {
+                request(server)
+                  .propfind('/remote.php/webdav')
+                  .set('content-type', 'application/xml')
+                  .expect(function(res) {
+                    let listing = support.directoryListing(res);
+                    let newEtag = listing[0].etag;
+                    assert.ok(newEtag, 'new directory listing should have etag set');
+                    assert.ok(etag != newEtag, 'etag should be different than before: ' + etag + ', ' + newEtag);
+                  })
+                  .expect(207, done);
+              });
+            }, 1000);
+          });
+      });
+  });
+});
---
+++ tests/server/support/index.js
@@ -0,0 +1,38 @@
+const os      = require('os');
+const http    = require('http');
+const fs      = require('fs');
+const express = require('express');
+const xmldoc  = require('xmldoc');
+const api     = require('../../../server/index');
+
+exports.makeApp = function() {
+  process.env.STORAGE_PATH = os.tmpdir() + 'davros-test-data-' + Math.random();
+  fs.mkdirSync(process.env.STORAGE_PATH);
+
+  let app = express();
+  let server = http.createServer(app);
+  api(app, {httpServer: server});
+  return app;
+};
+
+exports.xmlResponse = function(res) {
+  return new xmldoc.XmlDocument(res.text);
+};
+
+exports.directoryListing = function(doc) {
+  if(doc.text) {
+    doc = exports.xmlResponse(doc);
+  }
+
+  return doc.children.map(function(el) {
+    var prop = el.descendantWithPath('d:propstat.d:prop');
+    return {
+      href:          el.valueWithPath('d:href'),
+      lastmodified:  prop.valueWithPath('d:getlastmodified'),
+      contentLength: prop.valueWithPath('d:getcontentlength'),
+      contentType:   prop.valueWithPath('d:getcontenttype'),
+      etag:          prop.valueWithPath('d:getetag'),
+      isCollection:  !!prop.descendantWithPath('d:resourcetype.d:collection')
+    };
+  });
+}
