2633d33b24518a43288f684bb7a6fc429c51d124
Michael Nutt <michael@nuttnet.net>
2016-07-31 18:51:22 -0400

    wip

--- app/routes/file.js
+++ app/routes/file.js
@@ -3,6 +3,7 @@ import fetch from 'ember-network/fetch';
 import $ from 'jquery';
 import File from 'davros/models/file';
 import ensureCollectionExists from 'davros/lib/ensure-collection-exists';
+import { task } from 'ember-concurrency';
 
 export default Ember.Route.extend({
 
@@ -42,6 +43,40 @@ export default Ember.Route.extend({
     }
   },
 
+  uploadFile: task(function * (file) {
+    console.log(file.get('name'), file.blob.type);
+    if(file.blob.type === "") { yield; } // it's a directory
+    var location = document.location.pathname;
+    var path = file.blob.webkitRelativePath || file.get('name');
+
+    if(location.indexOf('/files') === 0) {
+      // if user is in a directory, upload the files there
+      location = location.replace(/^\/files\//, '');
+      // dirname of current path, so if path is /foo/README, use /foo/
+      location = location.replace(/\/[^\/]*$/, '');
+    } else {
+      // otherwise, upload files in the root directory
+      // (this shouldn't happen anymore)
+      location = '';
+    }
+
+    if(path[0] !== '/') { path = '/' + path; }
+
+    console.log("uploading " + path + " into location " + location);
+
+    var fullPath = [location, path].join('');
+
+    yield ensureCollectionExists(fullPath).then(() => {
+      return file.upload('/api/upload', {
+        data: {
+          destination: fullPath
+        }
+      }).then(() => {
+        return this.get('controller.model').load();
+      });
+    });
+  }).maxConcurrency(5).enqueue(),
+
   actions: {
     "delete": function() {
       var model = this.get('controller.model');
@@ -57,7 +92,7 @@ export default Ember.Route.extend({
       }
     },
 
-    newDirectory: function() {
+    newDirectory() {
       var model = this.get('controller.model');
 
       var dirname = prompt("Directory name");
@@ -77,48 +112,21 @@ export default Ember.Route.extend({
       }
     },
 
-    chooseUpload: function() {
+    chooseUpload() {
       $("#upload-placeholder").click();
     },
 
-    downloadDirectory: function() {
+    downloadDirectory() {
       var path = this.get('controller.model.path');
       var endpoint = "/api/archive?path=" + encodeURIComponent(path);
       document.location.href = endpoint;
     },
 
-    uploadFile: function (file) {
-      var source = file.file.getSource();
-      var location = document.location.pathname;
-      var path = source.relativePath || file.get('name');
-
-      if(location.indexOf('/files') === 0) {
-        // if user is in a directory, upload the files there
-        location = location.replace(/^\/files\//, '');
-        // dirname of current path, so if path is /foo/README, use /foo/
-        location = location.replace(/\/[^\/]*$/, '');
-      } else {
-        // otherwise, upload files in the root directory
-        // (this shouldn't happen anymore)
-        location = '';
-      }
-
-      if(path[0] !== '/') { path = '/' + path; }
-
-      console.log("uploading " + path + " into location " + location);
-
-      var fullPath = [location, path].join('');
-
-      ensureCollectionExists(fullPath).then(() => {
-        file.upload('/api/upload', {
-          data: {
-            destination: fullPath
-          }
-        }).then(() => {
-          this.get('controller.model').load();
-        });
-      });
+    upload: function(file) {
+      console.log("UPLOAD");
+      this.get('uploadFile').perform(file);
     }
+
   }
 
 });
