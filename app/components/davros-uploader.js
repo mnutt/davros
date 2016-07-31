98f46ca378cb0bef16d2b21632fcbb2e8ab7cf5f
Michael Nutt <michael@nuttnet.net>
2016-07-31 11:03:44 -0400

    override a bunch of pluploader stuff to get it to work with ember 2.8

--- app/components/davros-uploader.js
+++ app/components/davros-uploader.js
@@ -2,14 +2,69 @@ import plUploader from 'ember-plupload/components/pl-uploader';
 import jQuery from 'jquery';
 import Ember from 'ember';
 
+const { get, set } = Ember;
+
 export default plUploader.extend({
+  BASE_URL: '/assets/',
+
+  fixConfig: Ember.on('didInsertElement', function() {
+    delete this.get('config').required_features.send_browser_cookies;
+  }),
+
+  dropzoneElement() {
+    return jQuery(`#${get(this, 'dropzone.id')}`);
+  },
+
+  attachUploader() {
+    var uploader = get(this, 'uploader');
+    var queue = uploader.findOrCreate(get(this, 'name'), this, get(this, 'config'));
+    set(this, 'queue', queue);
+
+    // Send up the pluploader object so the app implementing this component as has access to it
+    var pluploader = queue.get('queues.firstObject');
+    this.sendAction('onInitOfUploader', pluploader);
+    this._dragCounter = 0;
+    this._invalidateDragData();
+  },
+
+  enteredDropzone(evt) {
+    var e = evt.originalEvent;
+    if (e.preventDefault) { e.preventDefault(); }
+    if (e.stopPropagation) { e.stopPropagation(); }
+    if (this._dragCounter === 0) {
+      this.activateDropzone(evt.originalEvent);
+    }
+    this._dragCounter++;
+  },
+
+  leftDropzone(evt) {
+    console.log('left');
+    var e = evt.originalEvent;
+    if (e.preventDefault) { e.preventDefault(); }
+    if (e.stopPropagation) { e.stopPropagation(); }
+    this._dragCounter--;
+    if (this._dragCounter === 0) {
+      this.deactivateDropzone();
+    }
+  },
+
   activateDropzone(evt) {
-    jQuery(`#${Ember.get(this, 'dropzone.id')}`).addClass('dropzone-active');
-    Ember.set(this, 'dragData', Ember.get(evt, 'dataTransfer'));
+    console.log('activated');
+    this.dropzoneElement().addClass('dropzone-active');
+    set(this, 'dragData', get(evt, 'dataTransfer'));
   },
 
   deactivateDropzone() {
-    jQuery(`#${Ember.get(this, 'dropzone.id')}`).removeClass('dropzone-active');
-    Ember.set(this, 'dragData', null);
-  }
+    console.log('deactivated');
+    this._dragCounter = 0;
+    this.dropzoneElement().removeClass('dropzone-active');
+    set(this, 'dragData', null);
+  },
+
+  detachUploader: Ember.on('willDestroyElement', function () {
+    var queue = get(this, 'queue');
+    if (queue) {
+      queue.orphan();
+    }
+  })
 });
