a520d261ce1f1fefcd11fb14ac5862c854129e1e
Michael Nutt <michael@nuttnet.net>
2020-02-20 12:37:49 -0500

    prettify more directories

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ var startTime = new Date();
 require('cache-require-paths');
 
 var express = require('express');
-var fs      = require('fs');
-var api     = require('./server');
-var path    = require('path');
-var http    = require('http');
+var fs = require('fs');
+var api = require('./server');
+var path = require('path');
+var http = require('http');
 var compression = require('compression');
 
 var root = __dirname;
 var indexFile = path.resolve(root + '/dist/index.html');
 
 fs.access(indexFile, fs.constants.F_OK, function(err) {
-  if(err) {
-    console.error("Missing dist/index.html; run `ember build` to generate it.");
+  if (err) {
+    console.error('Missing dist/index.html; run `ember build` to generate it.');
     process.exit(1);
   }
 });
@@ -24,7 +24,7 @@ var server = http.createServer(app);
 app.use(compression());
 app.use(express.static('dist'));
 
-api(app, {httpServer: server});
+api(app, { httpServer: server });
 
 app.use('/', function(req, res, next) {
   // send ember's index.html for any unknown route
@@ -34,12 +34,12 @@ app.use('/', function(req, res, next) {
 var port = process.env.PORT || 8000;
 var socket = process.env.SOCKET;
 
-if(socket) {
+if (socket) {
   server.listen(socket, function() {
     console.log('Davros listening on %s', socket);
   });
 } else {
-  server.listen(port, function () {
+  server.listen(port, function() {
     var time = new Date() - startTime;
     console.log('Davros started in %sms, listening on port %s', time, port);
   });
--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,6 +1,4 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
-  workflow: [
-    { handler: "silence", matchId: "ember-views.event-dispatcher.mouseenter-leave-move" }
-  ]
+  workflow: [{ handler: 'silence', matchId: 'ember-views.event-dispatcher.mouseenter-leave-move' }]
 };
