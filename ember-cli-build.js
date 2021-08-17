fd02de366a4c339204dd2373bb924d74bf3d46c7
Michael Nutt <michael@nuttnet.net>
2021-08-16 21:13:07 -0400

    properly specify purgeCSS safelist

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,7 +13,7 @@ const purgeCSS = {
       './app/components/**/*.hbs',
       './app/templates/**/*.hbs'
     ],
-    safelistPatterns: [/pswp/],
+    safelist: [/pswp/],
     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
   }
 };
