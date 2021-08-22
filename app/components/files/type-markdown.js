f9a08df0d99397a765721ee352b53ce0c9e21247
Michael Nutt <michael@nuttnet.net>
2021-08-22 13:22:09 -0400

    no getter side effects; clean up 'no preview' output

--- app/components/files/type-markdown.js
+++ app/components/files/type-markdown.js
@@ -1,26 +1,26 @@
 import Component from '@glimmer/component';
 import showdown from 'showdown';
-import { tracked } from '@glimmer/tracking';
 
 const markdownOptions = {
   completeHTMLDocument: false,
   openLinksInNewWindow: true,
-  tables: true
+  tables: true,
 };
 
 const style =
   '<style>html { font-family: sans-serif; padding: 20px; } pre { white-space: pre-wrap; }</style>\n\n';
 
 export default class TypeMarkdownComponent extends Component {
-  @tracked errored = false;
-
   get canSandbox() {
     return 'sandbox' in document.createElement('iframe');
   }
 
+  get errored() {
+    return this.srcDoc === '';
+  }
+
   get srcDoc() {
     if (this.args.model.previewFailed) {
-      this.errored = true;
       return '';
     }
 
@@ -28,7 +28,6 @@ export default class TypeMarkdownComponent extends Component {
       const converter = new showdown.Converter(markdownOptions);
       return style + converter.makeHtml(this.args.model.previewContent);
     } catch (e) {
-      this.errored = true;
       return '';
     }
   }
