a3e37db9dd580ff65091eda00a160451968efe2a
Michael Nutt <michael@nuttnet.net>
2021-08-28 14:42:35 -0400

    modals only cover app, not document.body (makes testing easier)

--- app/components/modal-content.js
+++ app/components/modal-content.js
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
 
 export default class ModalContentComponent extends Component {
-  defaultContainer = document.body;
+  defaultContainer = document.querySelector('.ember-application');
 }
