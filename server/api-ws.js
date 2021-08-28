4067213d93ef111de6a87f91e0d49b0d086bb762
Michael Nutt <michael@nuttnet.net>
2021-08-28 14:45:34 -0400

    start a _real_ backend for tests, using a temp dir

--- server/api-ws.js
+++ server/api-ws.js
@@ -17,13 +17,15 @@ exports.notify = function (file) {
 exports.serve = function (server) {
   wss = new WebSocket.Server({ noServer: true });
 
-  server.on('upgrade', function upgrade(request, socket, head) {
-    if (request.url.startsWith('/ws-files')) {
-      wss.handleUpgrade(request, socket, head, function done(ws) {
-        wss.emit('connection', ws, request);
-      });
-    }
-  });
+  if (server) {
+    server.on('upgrade', function upgrade(request, socket, head) {
+      if (request.url.startsWith('/ws-files')) {
+        wss.handleUpgrade(request, socket, head, function done(ws) {
+          wss.emit('connection', ws, request);
+        });
+      }
+    });
+  }
 
   wss.on('error', (err) => console.error(err));
 };
