14b3719a53d4bff688808fd5a8255bbafec81e05
Michael Nutt <michael@nuttnet.net>
2021-08-28 21:57:59 -0400

    bump node to 16

--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -5,9 +5,12 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-curl -sL https://deb.nodesource.com/setup_15.x | bash -
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
 apt-get update
-apt-get install -y --no-install-suggests --no-install-recommends nodejs git-core g++ libreoffice-writer libreoffice-impress libreoffice-calc unoconv
+apt-get install -y --no-install-suggests --no-install-recommends clang++-7 clang-7 nodejs git-core g++ libreoffice-writer libreoffice-impress libreoffice-calc unoconv
+
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-7 100
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-7 100
 
 # Set up libreoffice config directory
 rm -Rf /var/libreoffice
