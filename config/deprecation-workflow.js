60525f43de7af3704e2ae88d698f176c16ccbe27
Michael Nutt <michael@nuttnet.net>
2021-12-25 15:06:39 -0500

    remove old deprecations; silence a new one

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,10 +1,5 @@
 /* eslint-disable no-undef */
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
-  workflow: [
-    { handler: 'silence', matchId: 'deprecated-run-loop-and-computed-dot-access' },
-    { handler: 'silence', matchId: 'ember-global' },
-    { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' },
-    { handler: 'silence', matchId: 'routing.transition-methods' },
-  ],
+  workflow: [{ handler: 'silence', matchId: 'ember-polyfills.deprecate-assign' }],
 };
