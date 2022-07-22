9b024fc3195e74cd86da98aaa34e07d027abb872
Stephen Pollard <stephen@stephentpollard.com>
2022-07-21 22:46:54 -0400

    Decode uri before ensuring folder creation

--- app/components/davros-uploader.js
+++ app/components/davros-uploader.js
@@ -46,7 +46,11 @@ export default class DavrosUploader extends Component {
 
     var fullPath = ['/', this.uploadLocation, path].join('');
 
-    yield File.ensureCollectionExists(fullPath).then(() => {
+    // THis SAVES IT!!!
+    // yield File.ensureCollectionExists('/').then(() => {
+    // yield File.ensureCollectionExists(fullPath).then(() => {
+    // yield File.ensureCollectionExists(fullPath.replace("%20"," ")).then(() => {
+    yield File.ensureCollectionExists(decodeURI(fullPath)).then(() => {
       return file.upload('/api/upload', {
         data: {
           destination: fullPath,
