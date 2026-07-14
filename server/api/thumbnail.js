bd00bf0835598512956f75043ee2c139e0000a2f
Michael Nutt <michael@nuttnet.net>
2026-07-14 04:11:56 +0000

    thumbnail sets default dimensions before clamping

--- server/api/thumbnail.js
+++ server/api/thumbnail.js
@@ -8,8 +8,15 @@ const fits = {
   contain: 'contain'
 };
 
+const DEFAULT_DIMENSION = 100;
+
 function clamp(value, min, max) {
   return Math.max(Math.min(value, max), min);
+}
+
+function dimension(raw) {
+  const value = parseInt(raw, 10);
+  return clamp(Number.isNaN(value) ? DEFAULT_DIMENSION : value, 1, 3000);
 }
 
 module.exports = function(davServer) {
@@ -17,8 +24,8 @@ module.exports = function(davServer) {
     const sharp = require('sharp');
 
     const { searchParams } = new URL(req.url, 'http://dummy');
-    const width = clamp(parseInt(searchParams.get('width'), 10), 1, 3000);
-    const height = clamp(parseInt(searchParams.get('height'), 10), 1, 3000);
+    const width = dimension(searchParams.get('width'));
+    const height = dimension(searchParams.get('height'));
 
     const position = 'center';
     const fit = fits[searchParams.get('fit')] || 'cover';
